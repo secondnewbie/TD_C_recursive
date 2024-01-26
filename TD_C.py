6da9d7fe17df15bd15036e4a8d38e8200ed20f85
juno.park <pjguam0@naver.com>
2024-01-26 17:49:16 +0900

    modify final of final

--- TD_C.py
+++ TD_C.py
@@ -17,7 +17,7 @@ file_data = {
                 }
             },
             'EP0002': {
-                'EP002_0010': {
+                'EP0002_0010': {
                     'fps': 24,
                     'frange': [1001, 1050],
                     'author': 'anon',
@@ -62,36 +62,37 @@ def json_delete(data1: dict, data2: dict):
 
 def json_modify(data: dict, key_lst: list, val) -> None:
     """
-    :param data: 값을 바꿀 딕셔너리
-    :param key_lst: 값을 바꿀 키 경로(리스트)
-    :param val: 변경할 밸류 값
+    :param data: 원본 데이터 (json)
+    :param key_lst: 바꿀 값의 경로
+    :param val: 바꿀 밸류 값
     """
-    for key in key_lst[:-1]:
-        if key in data and isinstance(data[key], dict):  # 바꾸려는 키값이 data에 포함된 동시에 딕셔너리인지 확인
-            data = data[key]  # 딕셔너리 탐색을 위해 현재 키 값을 'data'로 업데이트
-        else:
+    for key in key_lst:
+        last_key = key_lst[-1]
+        if last_key in data:
+            data[last_key] = val
             return
-
-    last_key = key_lst[-1]  # 마지막 키 값을 변수로 지정
-    if last_key in data:
-        data[last_key] = val
-
-    pprint.pprint(file_data)  # 결과 프린트
+        if key in data and isinstance(data[key], dict):
+            json_modify(data[key], key_lst[1:], val)
 
 
 insert_key_data = {'project': {'shot': {'EP0005': {'EP0005_0050': {'frange': [1001, 1200]}}}}}
 key_data = ['project', 'shot', 'EP0001', 'EP0001_0010', 'frange']
 
 # Insert
+print("-"*110, "\n")
+print("Insert Result", "\n")
 json_insert(file_data, insert_key_data)
 pprint.pprint(file_data)
 
 # Delete
+print("-"*110, "\n")
+print("Delete Result", "\n")
 delete_key_data = {'project': {'shot': {'EP0005': None}}}
 json_delete(file_data, delete_key_data)
 pprint.pprint(file_data)
 
-
 # Modify
+print("-"*110, "\n")
+print("Modify Result", "\n")
 json_modify(file_data, key_data, [4321, 1234])
-
+pprint.pprint(file_data)
