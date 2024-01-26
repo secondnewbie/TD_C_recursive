78ef29d57f5963fa065c8bbab1454e7fa726b2ab
sj <tjdtnwls415@gmail.com>
2024-01-26 09:53:26 +0900

    add insert, delete

--- TD_C.py
+++ TD_C.py
@@ -27,22 +27,43 @@ file_data = {
 # key_data = {'project': {'shot': {'EP0001': {'EP0001_0010': {'frange': None}}}}}
 
 
-class recursive:
-    def json_get(self):
-        pass
+# class recursive:
+def json_get(self):
+    pass
 
-    def json_insert(self):
-        pass
+def json_insert(data1:dict, data2:dict):
+    for k, v in data1.items():
+        for i, j in data2.items():
+            if isinstance(v, dict):
+                if isinstance(j, dict):
+                    if k != i:
+                        data1[i] = j
+                        # print(data1)
+                        return
+                    json_insert(v, j)
 
-    def json_delete(self):
-        pass
+def json_delete(data1:dict, data2:dict):
+    for k, v in data1.items():
+        for i, j in data2.items():
+            if i == 'EP0005':
+                data1[i] = None
+                print(data1)
+                return
+            if isinstance(v, dict):
+                if isinstance(j, dict):
+                    json_delete(v, j)
 
-    def json_modify(self):
-        pass
+
+def json_modify(self):
+    pass
 
 insert_key_data = {'project': {'shot': {'EP0005': {'EP0005_0050': {'frange': [1001, 1200]}}}}}
-# json_insert(file_data, insert_key_data)
+json_insert(file_data, insert_key_data)
+print(file_data)
+
 json_modify(file_data, ['project', 'shot', 'EP0001', 'EP0001_0010', 'frange'], [1001, 1100])
 json_modify(file_data, key_data, [1001, 1100])
+
 delete_key_data = {'project': {'shot': {'EP0005': None}}}
 json_delete(file_data, delete_key_data)
+print(file_data)
